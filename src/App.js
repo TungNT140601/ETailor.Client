1009b56ea6ab73e9ea32e69d7f66ba606957de28
DaoAnhTu2105 <tudase151149@gmail.com>
2024-01-22 10:56:20 +0700

    add react query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { RouterComponents } from './router';
+import { RouterComponents } from "./router";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <div className='test' style={{
-      fontFamily: 'Open Sans, sans - serif'
-    }}>
-      <RouterComponents />
-    </div >
+    <QueryClientProvider client={queryClient}>
+      <div
+        className="test"
+        style={{
+          fontFamily: "Open Sans, sans - serif",
+        }}
+      >
+        <RouterComponents />
+      </div>
+    </QueryClientProvider>
   );
 }
 
